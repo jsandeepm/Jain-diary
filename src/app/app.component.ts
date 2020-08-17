ebae14eaf2cb9f2b29b62bac88fc81ee867c5888
Sandeep.M <Sandeep.M@non.se.com>
2020-08-17 09:15:01 +0530

    update menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+import {faCoffee, faHospitalUser, faMusic, faTheaterMasks} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jain-diary';
+  faCoffee = faCoffee;
+  faHospitalUser = faHospitalUser;
+  faMusic = faMusic;
+  faTheaterMasks = faTheaterMasks;
 }
